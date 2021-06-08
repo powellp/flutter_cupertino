9ff01cdf7b29b68aa2707a824f51617b8cba33a6
powellp <prashant.powell@gmail.com>
2021-06-08 18:25:20 +0545

    welcome_screen,login_screen,signup_screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_auth/Screens/Welcome/welcome_screen.dart';
+import 'package:flutter_auth/constants.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  void answerQuestion() {
-    print('anser choosen');
-  }
-
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      'What\'s your favrouite colour?',
-      'What\'s is your favrouite animal?',
-    ];
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('My first App'),
-        ),
-        body: Column(
-          children: [
-            Text(questions.elementAt(0)),
-            RaisedButton(
-              child: Text('Answer 1'),
-              onPressed: answerQuestion,
-            ),
-            RaisedButton(
-              child: Text('Answer 2'),
-              onPressed: () => print('answer 2 choosen'),
-            ),
-            RaisedButton(
-              child: Text('Answer 3'),
-              onPressed: () {
-                print('answer 3 choosen');
-              },
-            ),
-          ],
-        ),
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Auth',
+      theme: ThemeData(
+        primaryColor: kPrimaryColor,
+        scaffoldBackgroundColor: Colors.white,
       ),
+      home: WelcomeScreen(),
     );
   }
 }
